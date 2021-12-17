3b3b502977a0cde5ba88594770d08f5cf88cae52
Afrie Irham <afrieirham.work@gmail.com>
2021-12-17 23:47:45 +0800

    Revalidate every minute

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export const getStaticProps = async () => {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 10 seconds
-    revalidate: 60 * 60 * 1, // In seconds
+    revalidate: 60, // In seconds
   }
 }
 

7580dc5aed41331bfb1708f958fdb54aff24f7e1
Afrie Irham <afrieirham.work@gmail.com>
2022-02-24 23:21:35 +0800

    Fix no `title` in `_document.js`

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import React from 'react'
 
 import { customTheme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <React.Fragment>
+      <Head>
+        <title>Koleksi Resepi Khairulaming</title>
+      </Head>
+      <ChakraProvider theme={customTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </React.Fragment>
   )
 }
 

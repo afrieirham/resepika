958f2e96589c2b3e5e001ac10261674070ad5e9e
Afrie Irham <afrieirham.work@gmail.com>
2022-04-07 07:34:06 +0800

    Only recipes are scrollable

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, Image, Input, Link, Text } from '@chakra-ui/react'
+import { Box, Flex, Grid, Hide, Image, Input, Link, Show, Text } from '@chakra-ui/react'
 import { useState } from 'react'
 
 import { client } from '../utils/contentful'
@@ -38,59 +38,87 @@ export default function Home({ recipes }) {
     setFiltered(filtered)
   }
 
+  const renderItems = () =>
+    filtered.map(({ fields }) => {
+      const hasImage = Boolean(fields.thumbnail?.fields.file.url)
+      return (
+        <Flex
+          as={Link}
+          href={fields.postUrl}
+          isExternal
+          _hover={{ textDecoration: 'none' }}
+          key={fields.postUrl}
+          boxShadow='md'
+          borderRadius='md'
+          bg='white'
+          w={{ base: 'sm', md: 'full' }}
+          mx='auto'
+          my={{ base: '2', md: '0' }}
+        >
+          <Image
+            maxW='120px'
+            maxH='120px'
+            minW='120px'
+            minH='120px'
+            src={hasImage ? fields.thumbnail.fields.file.url : '/recipe-placeholder.png'}
+            borderLeftRadius='md'
+            fit='cover'
+            boxShadow='md'
+          />
+          <Flex py='2' px='4' minH='full'>
+            <Text fontWeight='semibold' fontSize='md' noOfLines={4}>
+              {fields.title}
+            </Text>
+          </Flex>
+        </Flex>
+      )
+    })
+
+  const displayProps = {
+    h: '80vh',
+    w: 'full',
+    mx: { base: 'auto', lg: 0 },
+    overflow: 'scroll',
+    px: { base: '0', md: '6', lg: '8' },
+    py: '2',
+  }
+
   return (
-    <Box pt='6' px={{ base: '4', md: '6', lg: '8' }} bgColor='#EDF2F7'>
-      <Box mx='auto' w='full' maxW='sm'>
+    <Box bgColor='#EDF2F7'>
+      <Flex
+        direction='column'
+        h='15vh'
+        mx='auto'
+        w='full'
+        maxW='sm'
+        justifyContent='center'
+        alignItems='center'
+      >
         <Text fontSize='xl' fontWeight='bold' textAlign='center' mb='2'>
           Koleksi Resepi Khairulaming
         </Text>
         <Input placeholder='Cari resepi...' size='lg' bg='white' onChange={onSearch} />
-      </Box>
+      </Flex>
+
+      <Flex display={{ base: 'flex', md: 'none' }} direction='column' {...displayProps}>
+        {renderItems()}
+      </Flex>
+
       <Grid
-        mt='6'
-        w='full'
+        display={{ base: 'none', md: 'grid' }}
         templateColumns={{
           base: 'repeat(1, 1fr)',
           md: 'repeat(3, 1fr)',
           xl: 'repeat(4, 1fr)',
         }}
+        templateRows='repeat(10, 1fr)'
         gap='6'
-        maxW={{ base: 'sm', md: 'full' }}
-        mx={{ base: 'auto', lg: 0 }}
+        {...displayProps}
       >
-        {filtered.map(({ fields }) => {
-          const hasImage = Boolean(fields.thumbnail?.fields.file.url)
-          return (
-            <Flex
-              as={Link}
-              href={fields.postUrl}
-              isExternal
-              _hover={{ textDecoration: 'none' }}
-              key={fields.postUrl}
-              boxShadow='md'
-              borderRadius='md'
-              bg='white'
-            >
-              <Image
-                maxW='120px'
-                maxH='120px'
-                minW='120px'
-                minH='120px'
-                src={hasImage ? fields.thumbnail.fields.file.url : '/recipe-placeholder.png'}
-                borderLeftRadius='md'
-                fit='cover'
-                boxShadow='md'
-              />
-              <Flex py='2' px='4' minH='full'>
-                <Text fontWeight='semibold' fontSize='md' noOfLines={4}>
-                  {fields.title}
-                </Text>
-              </Flex>
-            </Flex>
-          )
-        })}
+        {renderItems()}
       </Grid>
-      <Flex py='4' justifyContent='center' alignItems='center'>
+
+      <Flex py='4' justifyContent='center' alignItems='center' h='5vh'>
         <Text color='gray.500' fontSize='xs' textAlign='center'>
           <Link href='https://afrieirham.com' isExternal>
             Made with ❤️ by Afrie

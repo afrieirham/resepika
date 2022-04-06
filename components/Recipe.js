6ac6cf9ac34a53366f993f309f1592757361e1f0
Afrie Irham <32460534+afrieirham@users.noreply.github.com>
2022-04-07 07:42:26 +0800

    Design/overflow-scroll (#2)
    * Only recipes are scrollable
    
    * Create recipe component
    
    * Fix key prop missing

---
+++ components/Recipe.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Flex, Image, Link, Text } from '@chakra-ui/react'
+
+function Recipe({ fields }) {
+  const hasImage = Boolean(fields.thumbnail?.fields.file.url)
+  return (
+    <Flex
+      as={Link}
+      href={fields.postUrl}
+      isExternal
+      _hover={{ textDecoration: 'none' }}
+      boxShadow='md'
+      borderRadius='md'
+      bg='white'
+      w={{ base: 'sm', md: 'full' }}
+      mx='auto'
+      my={{ base: '2', md: '0' }}
+    >
+      <Image
+        maxW='120px'
+        maxH='120px'
+        minW='120px'
+        minH='120px'
+        src={hasImage ? fields.thumbnail.fields.file.url : '/recipe-placeholder.png'}
+        borderLeftRadius='md'
+        fit='cover'
+        boxShadow='md'
+      />
+      <Flex py='2' px='4' minH='full'>
+        <Text fontWeight='semibold' fontSize='md' noOfLines={4}>
+          {fields.title}
+        </Text>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Recipe
+

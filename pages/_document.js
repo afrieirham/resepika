31a97a1d79185725d4d0c47b6a06e5b4ab729f17
Afrie Irham <afrieirham.work@gmail.com>
2023-10-08 22:59:09 +0800

    add beam analytics

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,11 @@ class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5654598090665312"
             crossOrigin="anonymous"
           ></script>
+          <script
+            src="https://beamanalytics.b-cdn.net/beam.min.js"
+            data-token="52a61c07-85f9-440d-b47b-e9d0764e3c43"
+            async
+          ></script>
         </Head>
         <Box bgColor="#fafaf9" as="body">
           <Main />

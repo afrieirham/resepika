e1a86a68063a1d4b55f1fd2bda2302beaef9763b
Afrie Irham <afrieirham.work@gmail.com>
2024-03-08 16:55:39 +0800

    initialize new next app for rewrite

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://resepika.com",
-  generateRobotsTxt: true,
-  exclude: ["/admin", "/api/*"],
-};
--- utils/promiseFormatter.js
+++
@@ -1,5 +0,0 @@
-export const promiseFormatter = (promise) => {
-  return promise
-    .then((data) => [data, undefined])
-    .catch((error) => Promise.resolve([undefined, error]))
-}

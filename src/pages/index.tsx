e6f52d8ef1db2cee7c8d74e9d6e58776fcd4ef22
Afrie Irham <afrieirham.work@gmail.com>
2024-03-08 18:06:29 +0800

    implement search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import { recipes } from "../../public/data/recipes";
 
 export default function Home() {
+  const [filtered, setFiltered] = useState(recipes);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const filtered = recipes.filter((item) => {
+      const currentRecipe = item.title.toLowerCase();
+      return currentRecipe.includes(searchTerm);
+    });
+
+    setFiltered(filtered);
+  }, [searchTerm]);
+
   return (
     <main className="flex flex-col max-w-screen-lg mx-auto ">
       <div className="flex flex-col w-full p-8 space-y-2">
@@ -8,13 +21,14 @@ export default function Home() {
           Koleksi Resepi KhairulAming
         </h1>
         <input
-          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value.toLocaleLowerCase())}
           placeholder="Cari resepi..."
           className="flex w-full h-10 max-w-sm px-3 py-2 mx-auto text-sm border rounded-md border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
         />
       </div>
       <div className="grid grid-cols-1 gap-2 px-4 sm:grid-cols-2 md:grid-cols-3">
-        {recipes.map((resepi) => (
+        {filtered.map((resepi) => (
           <a
             key={resepi.postUrl}
             href={resepi.postUrl}

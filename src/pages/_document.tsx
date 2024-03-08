2c93ed7c53684dc369726a6d3beb746ee7fcb6ca
Afrie Irham <afrieirham.work@gmail.com>
2024-03-09 02:54:39 +0800

    add beam analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="52a61c07-85f9-440d-b47b-e9d0764e3c43"
+          async
+        ></script>
+      </Head>
       <body className="bg-gray-50">
         <Main />
         <NextScript />

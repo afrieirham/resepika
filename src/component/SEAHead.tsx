1811e3f165b2a1124feaf89822c979415f7909c8
Afrie Irham <afrieirham.work@gmail.com>
2024-03-08 18:09:43 +0800

    update SEO

---
+++ src/component/SEAHead.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+
+function SEOHead({
+  title,
+  description,
+  path,
+  ogPath = "/og.png",
+}: {
+  title: string;
+  description: string;
+  path: string;
+  ogPath: string;
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="robots" content="index,follow" />
+      <link rel="canonical" href="https://resepika.com" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:site_name" content={title} />
+      <meta property="og:url" content={`https://resepika.com${path}`} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`https://resepika.com${ogPath}`} />
+
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:creator" content="@afrieirham_" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="typit.in" />
+      <meta property="twitter:url" content={`https://resepika.com${path}`} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={`https://resepika.com${ogPath}`} />
+    </Head>
+  );
+}
+
+export default SEOHead;

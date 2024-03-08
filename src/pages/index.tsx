28686fba7b4ebcf0bb090b9c1e80c726599d6895
Afrie Irham <afrieirham.work@gmail.com>
2024-03-09 01:42:12 +0800

    create resepi list component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import List from "@/component/List";
 import SEOHead from "@/component/SEAHead";
 import { recipes } from "../../public/data/recipes";
 
@@ -89,29 +90,7 @@ export default function Home() {
         ))}
       </div>
 
-      <div
-        className={`grid grid-cols-1 gap-2 px-4 mt-6 sm:px-0 sm:grid-cols-2 md:grid-cols-3 ${
-          loading ? "animate-pulse" : ""
-        }`}
-      >
-        {filtered.map((resepi) => (
-          <a
-            key={resepi.postUrl}
-            href={resepi.postUrl}
-            target="_blank"
-            className="relative flex flex-col p-4 space-y-2 transition-all duration-300 ease-out rounded-xl hover:bg-gray-100 hover:-translate-y-1"
-          >
-            <img
-              src={`/thumbnails/${resepi.thumbnail}`}
-              className="object-cover aspect-square max-h-[200px] rounded-md"
-            />
-            <p className="text-sm font-medium text-gray-900">{resepi.title}</p>
-          </a>
-        ))}
-      </div>
-      {filtered.length === 0 && (
-        <p className="text-center">Resepi tidak wujud</p>
-      )}
+      <List loading={loading} filtered={filtered} />
     </main>
   );
 }

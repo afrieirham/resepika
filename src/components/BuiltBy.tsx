2daaaf55538e378e15b862c7c2c5edf1980dc895
Afrie Irham <afrieirham.work@gmail.com>
2024-03-09 11:55:03 +0800

    add built by banner

---
+++ src/components/BuiltBy.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function BuiltBy() {
+  return (
+    <div className="fixed z-50 bottom-0 right-0">
+      <a
+        href="https://afrieirham.com"
+        target="_blank"
+        className="bg-yellow-300 text-yellow-900 p-2 rounded-t text-xs mx-4"
+      >
+        Built by @afrieirham
+      </a>
+    </div>
+  );
+}
+
+export default BuiltBy;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import BuiltBy from "@/components/BuiltBy";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -13,6 +14,7 @@ export default function Document() {
       <body className="bg-gray-50">
         <Main />
         <NextScript />
+        <BuiltBy />
       </body>
     </Html>
   );

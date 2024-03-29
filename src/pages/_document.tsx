89fb6003839d756fe88c02254b4a5c3c345f71bc
Afrie Irham <afrieirham.work@gmail.com>
2024-03-29 16:55:06 +0800

    add google adsense meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,8 @@ export default function Document() {
           src="https://beamanalytics.b-cdn.net/beam.min.js"
           data-token="52a61c07-85f9-440d-b47b-e9d0764e3c43"
           async
-        ></script>
+        />
+        <meta name="google-adsense-account" content="ca-pub-5654598090665312" />
       </Head>
       <body className="bg-gray-50">
         <Main />

6bf14b81d30589c2dd4a9cb679d69befb240420e
Afrie Irham <afrie@chip-in.asia>
2025-10-06 09:48:51 +0800

    remove beam analytics script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script
-          src="https://beamanalytics.b-cdn.net/beam.min.js"
-          data-token={process.env.NEXT_PUBLIC_BEAM_ANALYTICS_DATA_TOKEN}
-          async
-        />
-        <script
           defer
           src="https://analytics.afrieirham.com/script.js"
           data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
